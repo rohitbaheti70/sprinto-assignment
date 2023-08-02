0ac473fb9743ea64e1e86ba24029c98de26d5201
rohitbaheti70 <141215376+rohitbaheti70@users.noreply.github.com>
2023-08-02 13:02:57 +0530

    Update Js1.js

--- Js1.js
+++ Js1.js
@@ -1,3 +1,6 @@
 if(document.querySelectorAll('#footer').length>0){
 document.querySelector('#footer').innerText="Footer text changed";
 }
+else {
+  document.querySelector('#container').innerHTML="Page text changed";
+}
